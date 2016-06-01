8031693caa000bbf27dcd1e1b05f803a8ac4d94c
Héctor Orón Martínez <zumbi@debian.org>
2016-06-01 13:35:05 +0200

    1.10.0-1 (patches unapplied)
    Imported using git-ubuntu import.

--- src/git-version.h
+++ src/git-version.h
@@ -1,1 +1,1 @@
-#define BUILD_ID "1.9.91-7-g0cc4e98 configure.ac: bump to version 1.9.92 for the beta release (2016-02-02 15:34:56 -0800)"
+#define BUILD_ID "1.9.93-2-gd45de28 configure.ac: bump to version 1.10.0 for the official release (2016-02-16 12:37:43 -0800)"
