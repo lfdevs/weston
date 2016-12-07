91c5ce7e831ed5c370a0ef6a3efa7f6e130537dd
Emilio Pozuelo Monfort <pochu@debian.org>
2016-12-07 19:17:42 +0100

    1.12.0-1 (patches unapplied)
    Imported using git-ubuntu import.

--- libweston/git-version.h
+++ libweston/git-version.h
@@ -1,1 +1,1 @@
-#define BUILD_ID "1.11.93-7-g870f384 configure.ac: bump to version 1.11.94 for the RC2 release (2016-09-13 12:18:23 -0700)"
+#define BUILD_ID "1.11.94-2-ga08dff5 configure.ac: bump to version 1.12.0 for the official release (2016-09-20 12:22:46 -0700)"
